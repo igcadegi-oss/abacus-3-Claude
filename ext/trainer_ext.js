d576ba66d9e2efa168c09ab20037d47108f97124
igcadegi-oss <igcadegi@gmail.com>
2025-10-15 13:07:20 +0200

    Update trainer_ext.js

--- ext/trainer_ext.js
+++ ext/trainer_ext.js
@@ -1,20 +1,20 @@
+// ext/trainer_ext.js - Точка входа для тренажёра
 import { preloadSounds, playSound } from '../js/utils/sound.js';
 import { startTimer, stopTimer } from '../js/utils/timer.js';
-
-// Инициализация
-preloadSounds();
-
-// Проверка
-setTimeout(() => {
-  playSound('correct'); // должен сыграть звук
-  console.log('✅ Sound module loaded!');
-}, 1000);
-
-// ext/trainer_ext.js
 import { t } from "../core/i18n.js";
 import { state } from "../core/state.js";
 import { mountTrainerUI } from "./trainer_logic.js";
 
+// Инициализация звуков
+preloadSounds();
+
+// Проверочный тест (только для отладки)
+setTimeout(() => {
+  playSound('correct');
+  console.log('✅ Утилиты загружены: звуки и таймер работают');
+}, 1000);
+
+// Ждём, пока DOM загрузится
 (async () => {
   // Находим основной контейнер приложения
   const appRoot = document.getElementById("app");
@@ -23,6 +23,6 @@ import { mountTrainerUI } from "./trainer_logic.js";
     return;
   }
 
-  console.log("✅ TrainerView initialized on #app");
+  console.log("✅ TrainerView инициализирован в #app");
   mountTrainerUI(appRoot, { t, state });
 })();
